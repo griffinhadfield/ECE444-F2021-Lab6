37183b2134fc873e638c1e1b0533fb86510a5854
griffinhadfield <griffin.hadfield@mail.utoronto.ca>
2021-10-30 14:27:22 -0400

    added sql alchemy

--- create_db.py
+++ create_db.py
@@ -1,12 +1,12 @@
-# # create_db.py
+# create_db.py
 
 
-# from project.app import db
-# from project.models import Post
+from project.app import db
+from project.models import Post
 
 
-# # create the database and the db table
-# db.create_all()
+# create the database and the db table
+db.create_all()
 
-# # commit the changes
-# db.session.commit()
+# commit the changes
+db.session.commit()
